1111e32513a195893d121af32c3fe52016d2a6ee
daxiongxiongda <18701405276@163.com>
2018-11-26 15:03:01 +0800

    添加按钮
    添加按钮

--- githupTest/githupTest/ViewController.m
+++ githupTest/githupTest/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.redColor;
+    UIButton *butoon = [[UIButton alloc] init];
+    [self.view addSubview:butoon];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
